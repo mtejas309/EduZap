db1cb5c49e007d6f8159ccb3a2aec74270e01e06
TEJAS M <124518433+mtejas309@users.noreply.github.com>
2025-11-16 16:28:20 +0530

    Delete EduZap-Backend directory

--- EduZap-Backend/src/middleware/validateRequest.js
+++
@@ -1,9 +0,0 @@
-export const validateRequest = (req, res, next) => {
-  const { name, phone, title } = req.body;
-
-  if (!name || !phone || !title) {
-    return res.status(400).json({ error: "Name, phone & title are required." });
-  }
-
-  next();
-};
